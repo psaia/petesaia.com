eb900c7b4cbad80273efd7e0fc6e785892d243f2
Pete Saia <pete@lev-interactive.com>
2019-06-28 23:58:55 -0400

    Add better color channels

--- experiments/theremin/audio.js
+++ experiments/theremin/audio.js
@@ -82,6 +82,17 @@ class Composer {
         rTotal += image.data[i];
         gTotal += image.data[i + 1];
         bTotal += image.data[i + 2];
+
+        if (this.note === KEY_NOTES.r) {
+          image.data[i + 1] = 0;
+          image.data[i + 2] = 0;
+        } else if (this.note === KEY_NOTES.g) {
+          image.data[i] = 0;
+          image.data[i + 2] = 0;
+        } else {
+          image.data[i] = 0;
+          image.data[i + 1] = 0;
+        }
       }
 
       switch (this.note) {
